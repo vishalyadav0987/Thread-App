9973a2196440b5bdb339e86c67adea2bd715bdde
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-19 22:29:25 +0530

    Create the Model as UserSchema.

---
+++ backend/modals/UserShema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+    followers: {
+        type: [String],
+        default: [],
+    },
+    following: {
+        type: [String],
+        default: [],
+    },
+    bio: {
+        type: String,
+        default: "",
+    },
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('user', UserSchema);
