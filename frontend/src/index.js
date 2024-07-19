39e4556b9e5a95d800ca7a3b14145c06285a2f22
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-19 13:43:16 +0530

    Setup the chakra provider.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/provider'
+import { mode } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/theme-utils';
 
+
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('gray.100', '#101010')(props),
+    }
+  })
+}
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+}
+
+const color = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  }
+}
+
+const theme = extendTheme({ config, styles, color });
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
