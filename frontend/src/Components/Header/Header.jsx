6dc280c64de548bd77f6e8ce73b818c56713e6f2
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-19 15:21:41 +0530

    Creating Header add logo of thread app and add the logic of dark and ligh mode using useColorMode using chakra ui.

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useColorMode } from '@chakra-ui/react'
+
+const headerStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "0.8rem"
+};
+
+const imageStyle = {
+    width: "40px",
+    cursor: "pointer"
+}
+
+
+
+const Header = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
+    return (
+        <>
+            <div className="header" style={headerStyle}>
+                <img
+                    style={imageStyle}
+                    src={colorMode === "dark" ? "light-logo.svg" : "dark-logo.svg"}
+                    alt="Logo"
+                    onClick={toggleColorMode}
+                />
+            </div>
+        </>
+    )
+}
+
+export default Header
