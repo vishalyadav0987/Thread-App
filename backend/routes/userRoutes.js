1e618739265f45dacecbcbc214f48a781216fd62
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-21 12:02:53 +0530

    Set the login route as '/login' using post method.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {
-    register
+    register,
+    login
 } = require('../controllers/userController')
 
 
 router.route('/register').post(register)
+router.route('/login').post(login)
 
 
 module.exports = router
