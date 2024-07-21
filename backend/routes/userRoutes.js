7020d2cd87f89008cb8be54a85cc8f6a582ac884
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-21 21:22:33 +0530

    Set the followUnfollow routes as '/follow:id' by using post method.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,15 @@ const {
     register,
     login,
     logout,
+    followUnFollowUser,
 } = require('../controllers/userController');
+const { protectedRoute } = require('../middleware/protectedRoute')
 
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
+router.route('/follow/:id').post(protectedRoute, followUnFollowUser);
 
 
 module.exports = router
