1a68ec6761fad3407bd6415711a76e5e1b8507ce
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-21 12:09:13 +0530

    Set the login route as '/logout' using get method.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {
     register,
-    login
-} = require('../controllers/userController')
+    login,
+    logout,
+} = require('../controllers/userController');
 
 
-router.route('/register').post(register)
-router.route('/login').post(login)
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/logout').get(logout);
 
 
 module.exports = router
