2ba22e17870f3a57378e9034523afb4962126176
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-22 17:17:36 +0530

    Set delete post as '/:id' using delete method.

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { createPost,
     getPost,
+    deletePost,
 } = require('../controllers/postController');
 const router = express.Router();
 const { protectedRoute } = require('../middleware/protectedRoute')
 
 
-router.route('/:id').get(getPost);
+router.route('/:id').get(getPost).delete(protectedRoute, deletePost);
 router.route('/create').post(protectedRoute, createPost);
+
 
 module.exports = router;
