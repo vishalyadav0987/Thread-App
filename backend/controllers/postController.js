9c70d3d5175aff50e196657f5a1f48d74138fc03
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-22 16:16:41 +0530

    Create the createPost function and create the post taking data from the user from payload.

---
+++ backend/controllers/postController.js
@@ -0,0 +1,65 @@
+const PostSchema = require('../modals/PostSchema');
+const UserSchema = require('../modals/UserShema');
+
+//CREATE POST
+const createPost = async (req, res) => {
+    try {
+        const { postedBy, text, img } = req.body;
+
+        // idhar postedby ka maltalb hai ush user ki id jisne post ki hai
+        if (!postedBy || !text) {
+            return res.json({
+                success: false,
+                message: "Postedby and text fields are required."
+            })
+        }
+
+        const user = await UserSchema.findById(postedBy);
+
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "User not Found.",
+            })
+        }
+
+        if (user._id.toString() !== req.user._id.toString()) {
+            return res.json({
+                success: false,
+                message: "Un-Authorized to create post.",
+            })
+        }
+
+        const maxLength = 500;
+
+        if (text.length > maxLength) {
+            return res.json({
+                success: false,
+                message: `Text must be less than ${maxLength} characters.`
+            })
+        }
+
+        const newPost = new PostSchema({
+            postedBy,
+            text,
+            img
+        });
+
+
+        await newPost.save();
+
+        res.json({
+            success: true,
+            message: "Post Successfully posted.",
+            data: newPost,
+        })
+
+    } catch (error) {
+        console.log("Error in updateUser function ->", error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+module.exports = {
+    createPost,
+}
