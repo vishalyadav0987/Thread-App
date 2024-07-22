a32a3be9bcdeed19843d28e96f6d3d8df46b6994
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-22 16:53:32 +0530

    Set get single post as '/:id' using get method.

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createPost } = require('../controllers/postController');
+const { createPost,
+    getPost,
+} = require('../controllers/postController');
 const router = express.Router();
 const { protectedRoute } = require('../middleware/protectedRoute')
 
 
+router.route('/:id').get(getPost);
 router.route('/create').post(protectedRoute, createPost);
 
 module.exports = router;
