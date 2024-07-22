6f426855834b7e4288a1604b2a9bb8b340f483e2
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-22 19:50:40 +0530

    Create the register card using chakra ui form template.

---
+++ frontend/src/Components/RegisterCard/RegisterCard.jsx
@@ -0,0 +1,97 @@
+'use client'
+
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Link,
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+
+export default function RegisterCard({ setAuthForm }) {
+    const [showPassword, setShowPassword] = useState(false)
+
+    return (
+        <Flex
+            align={'center'}
+            justify={'center'}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'} textAlign={'center'}>
+                        Sign up
+                    </Heading>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <Stack spacing={4}>
+                        <HStack>
+                            <Box>
+                                <FormControl id="firstName" isRequired>
+                                    <FormLabel>Full Name</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                            <Box>
+                                <FormControl id="lastName" isRequired>
+                                    <FormLabel>Username</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                        </HStack>
+                        <FormControl id="email" isRequired>
+                            <FormLabel>Email address</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
+                        <FormControl id="password" isRequired>
+                            <FormLabel>Password</FormLabel>
+                            <InputGroup>
+                                <Input type={showPassword ? 'text' : 'password'} />
+                                <InputRightElement h={'full'}>
+                                    <Button
+                                        variant={'ghost'}
+                                        onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                        </FormControl>
+                        <Stack spacing={10} pt={2}>
+                            <Button
+                                loadingText="Submitting"
+                                size="lg"
+                                style={{ border: "1px solid #FFF" }}
+                                bg={useColorModeValue("gray.600", "gray.700")}
+                                color={'white'}
+                                _hover={{
+                                    bg: useColorModeValue("gray.700", "gray.800"),
+                                }}>
+                                Sign up
+                            </Button>
+                        </Stack>
+                        <Stack pt={6}>
+                            <Text align={'center'}>
+                                Already have an Account? <Link
+                                    onClick={() => { setAuthForm("Login") }}
+                                    color={'blue.400'}>Sign Up</Link>
+                            </Text>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
+    )
+}
