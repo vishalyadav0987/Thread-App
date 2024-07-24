035786dac3edc9d9bc193c68d44a24d30be14618
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-24 22:05:24 +0530

    Configure CORS for frontend and set up Cloudinary credentials
    - Set up CORS to allow requests from frontend (http://localhost:3001) with credentials support.
    - Configure Cloudinary with environment variables for image uploading.

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,28 @@ const app = express();
 const cors = require('cors');
 const connectDB = require('./connectDB/connect');
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const cloudinary = require('cloudinary').v2;
+// const PORT = process.env.PORT || 5000;
+const PORT = 3000 || 5000;
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/userRoutes')
 const postRoutes = require('./routes/postRoutes')
 
 
-app.use(cors());
 app.use(express.json());// parse payload data
 app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:3001', // Frontend URL
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY
+})
 
 // Routes
 app.use('/api/v1/user', userRoutes)
