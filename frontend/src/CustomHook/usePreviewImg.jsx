4d3b19e17bb6e9fa56b4adb972ec2d744e3e5852
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-24 22:15:55 +0530

    Add custom hook for image preview with validation and toast notifications
    - Created a custom hook, usePreviewImg, for managing image file preview.
    - Used useState to manage the imageUrl state, which holds the base64-encoded image data.
    - Implemented imageChangeHandle function to handle file input changes, validate if the file is an image, and generate a preview.
    - Utilized FileReader to convert the image file to a data URL for preview purposes.
    - Displayed an error toast message with react-hot-toast if the selected file is not an image.
    - Ensured the imageUrl state is updated with the preview data or set to null if the file format is incorrect.

---
+++ frontend/src/CustomHook/usePreviewImg.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import toast from "react-hot-toast";
+
+const usePreviewImg = () => {
+    const [imageUrl, setImageUrl] = useState("");
+    const imageChangeHandle = (e) => {
+        const file = e.target.files[0];
+        if (file && file.type.startsWith('image/')) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImageUrl(reader.result);
+            }
+            reader.readAsDataURL(file);
+        }
+        else {
+            toast.error("Please Check formate of Image.");
+            setImageUrl(null);
+        }
+    }
+
+    return { imageChangeHandle, imageUrl }
+}
+
+export default usePreviewImg
