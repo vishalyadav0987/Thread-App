0c34b56bb5fbd0e11d9f2c96b291542dd336d14a
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-24 21:55:33 +0530

    Change the option object and chage the expires time jwt. and remove the sameSite="strict" to "none".

--- backend/generateToken/generateToken.js
+++ backend/generateToken/generateToken.js
@@ -1,17 +1,20 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-const generateTokenAndSetCookie = (userId, res) => {
+const generateTokenAndSetCookie = async (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECERET, {
-        expiresIn: process.env.JWT_LIFETIME,
+        expiresIn: "7d",
     });
 
-    res.cookie("token", token, {
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
         httpOnly: true,
-        maxAge: 30 * 24 * 60 * 1000,
-        sameSite: "strict",
-    });
+    };
 
-    return token;
+
+    res.cookie("token", token, options)
 }
 
 module.exports = {
