9322f740e63a9bafbb004e26149db08d2a69e0b7
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-24 22:08:28 +0530

    Create AuthContext for managing authentication state
    - Defined AuthContext using createContext from React to manage authentication globally.
    - Created a custom hook, useAuthContext, for easy access to AuthContext values.
    - Implemented AuthContextProvider to wrap application components, providing them with authentication state.
    - Used useState to manage authUser state, initializing it with data from localStorage if available.
    - Provided authUser and setAuthUser functions through AuthContext to allow components to access and update authentication state.
    - Ensured children components within AuthContextProvider have access to authUser and setAuthUser for seamless state management.

---
+++ frontend/src/Context/AuthContext.js
@@ -0,0 +1,11 @@
+import { createContext, useContext, useState } from 'react';
+export const AuthContext = createContext();
+
+export const useAuthContext = () => {
+    return useContext(AuthContext);
+}
+
+export const AuthContextProvider = ({ children }) => {
+    const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("user-threads")) || null);
+    return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>
+}
