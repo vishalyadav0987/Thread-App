c5dbea0df5e0671f45cb50a04df3b6f0328f0c10
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-25 13:27:31 +0530

    Fetch and display user profile data with error handling
    - Added state management with useState to store user data.
    - Utilized useParams to extract the username from the URL for API requests.
    - Implemented useEffect to fetch user profile data from the backend when the username changes.
    - Made an asynchronous Axios GET request to `http://localhost:3000/api/v1/user/profile/${username}` to retrieve user profile information.
    - Displayed error notifications with react-hot-toast if the API response indicates failure.
    - Set the user state with the retrieved data on a successful response.
    - Handled potential errors in the data fetching function and logged them for debugging.
    - Ensured the component returns null if no user data is available to avoid rendering issues.

--- frontend/src/Pages/UserPage/UserPage.jsx
+++ frontend/src/Pages/UserPage/UserPage.jsx
@@ -1,33 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserHeader from '../../Components/UserHeader/UserHeader'
 import UserPost from '../../Components/UserPost/UserPost'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 const UserPage = () => {
+  const [user, setUser] = useState(null);
+  const { username } = useParams()
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/v1/user/profile/${username}`
+        );
+        if (!response.data.success) {
+          toast.error(response.data.message, {
+            className: 'custom-toast', // Custom class for styling)
+          });
+        }
+        setUser(response.data.data);
+      } catch (error) {
+        console.log("Error in fetchUserData Function ->", error.message);
+      }
+    }
+    fetchUserData();
+  }, [username]);
+
+  if (!user) return null;
   return (
     <>
       <div className="userpage-section">
-        <UserHeader />
+        <UserHeader user={user} />
         <UserPost
           postImg={"/post1.png"}
           likes={100}
           replies={481}
           postTitle={"This is my first image."}
         />
-        <UserPost 
-        postImg={"/logo192.png"}
-        likes={170}
-        replies={451}
-        postTitle={"This is my second image."}/>
-        <UserPost 
-        postImg={"/light-logo.svg"}
-        likes={600}
-        replies={301}
-        postTitle={"This is my Third image."}/>
-        <UserPost 
-        postImg={"/logo512.png"}
-        likes={20000}
-        replies={41}
-        postTitle={"This is my fourth image."}/>
+        <UserPost
+          postImg={"/logo192.png"}
+          likes={170}
+          replies={451}
+          postTitle={"This is my second image."} />
+        <UserPost
+          postImg={"/light-logo.svg"}
+          likes={600}
+          replies={301}
+          postTitle={"This is my Third image."} />
+        <UserPost
+          postImg={"/logo512.png"}
+          likes={20000}
+          replies={41}
+          postTitle={"This is my fourth image."} />
       </div>
     </>
   )
