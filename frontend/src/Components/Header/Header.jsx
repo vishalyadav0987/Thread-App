dccc847b12df0ff00eda1da52ab434932607b622
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-25 17:34:46 +0530

    Add navigation links with icons
    - Imported Link from react-router-dom for navigation.
    - Imported FaHome and FaRegUserCircle icons from react-icons/fa for home and user profile navigation respectively.
    - Created a Link component for home navigation with FaHome icon.
    - Created a Link component for profile update navigation with FaRegUserCircle icon.
    - Set the size of the icons to 36px for better visibility and user interface consistency.

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useColorMode } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { FaHome } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const headerStyle = {
     display: "flex",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
     padding: "0.8rem"
 };
 
@@ -20,12 +23,18 @@ const Header = () => {
     return (
         <>
             <div className="header" style={headerStyle}>
+                <Link to={'/'}>
+                    <FaHome size={"36px"} />
+                </Link>
                 <img
                     style={imageStyle}
                     src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
                     alt="Logo"
                     onClick={toggleColorMode}
                 />
+                <Link to={'/update'}>
+                    <FaRegUserCircle size={"36px"} />
+                </Link>
             </div>
         </>
     )
