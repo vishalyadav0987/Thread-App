5cc90fdb53b1dd844f3b444bd74c5164a6e480a7
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-30 10:00:04 +0530

    ' Commit Description: Add Route for Retrieving Messages
    ' This commit adds a new route to the messageRoutes for retrieving messages between users.
    ' The route configuration includes:
    
    ' - Path: '/:otherUserId'
    ' - Method: GET
    ' - Middleware: protectedRoute to ensure the route is accessible only to authenticated users.
    ' - Controller: getMessage to handle the request and retrieve messages for the specified otherUserId.
    
    ' This setup allows authenticated users to fetch messages from conversations with a specific user.

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { protectedRoute } = require('../middleware/protectedRoute');
 const {
     sendMessage,
+    getMessage,
 } = require('../controllers/messageContoller');
 const router = express.Router();
 
 
 router.route('/').post(protectedRoute, sendMessage)
+router.route('/:otherUserId').get(protectedRoute, getMessage)
 
 module.exports = router;
