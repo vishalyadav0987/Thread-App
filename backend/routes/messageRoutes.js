376a0915cee0509a14441d7ee341c0da1d1fc6f7
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-30 10:58:11 +0530

    ' Commit Description: Add Route for Retrieving User Conversations
    ' This commit adds a new route to the messageRoutes for retrieving conversations associated with the authenticated user.
    ' The route configuration includes:
    
    ' - Path: '/conversation'
    ' - Method: GET
    ' - Middleware: protectedRoute to ensure the route is accessible only to authenticated users.
    ' - Controller: getConversation to handle the request and return all conversations for the logged-in user.
    
    ' This setup allows authenticated users to fetch their conversations, excluding the current user from the participants list.

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -3,10 +3,12 @@ const { protectedRoute } = require('../middleware/protectedRoute');
 const {
     sendMessage,
     getMessage,
+    getConversation,
 } = require('../controllers/messageContoller');
 const router = express.Router();
 
 
+router.route('/conversation').get(protectedRoute, getConversation)
 router.route('/').post(protectedRoute, sendMessage)
 router.route('/:otherUserId').get(protectedRoute, getMessage)
 
