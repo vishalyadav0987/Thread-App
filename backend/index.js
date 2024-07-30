759f7b779d19271cf4b94b4dacd10992dcc59b38
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-07-30 09:21:37 +0530

    ' Commit Description: Integrate Message Routes into Application
    ' This commit integrates the message routes into the main application by adding them to the Express app instance.
    ' The following changes are made:
    
    ' - Imported messageRoutes from the './routes/messageRoutes' file.
    ' - Registered messageRoutes under the path '/api/v1/message' using the app.use() method.
    
    ' This setup allows the application to handle message-related API requests under the specified route.

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,10 @@ const PORT = 3000 || 5000;
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/userRoutes')
 const postRoutes = require('./routes/postRoutes')
+const messageRoutes = require('./routes/messageRoutes')
 
 
-app.use(express.json({limit:"50mb"}));// parse payload data
+app.use(express.json({ limit: "50mb" }));// parse payload data
 app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(cookieParser())
 app.use(cors({
@@ -29,6 +30,7 @@ cloudinary.config({
 // Routes
 app.use('/api/v1/user', userRoutes)
 app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/message', messageRoutes)
 
 app.get('/test', (req, res) => {
     res.send("This test route for testing purpose.");
