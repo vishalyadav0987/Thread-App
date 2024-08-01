2b6d1b664cd42ceb304a186b43fc14116aee2171
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-08-01 12:26:37 +0530

    Commit description:Add markMessageSeen functionality and seen property to messages
    Implement markMessagesAsSeen functionality with real-time updates
    
    - Added `markMessagesAsSeen` event listener in the MessageContainer component to mark messages as seen when the user views them.
    - Included logic in `useEffect` to emit `markMessagesAsSeen` event if the last message is from another user.
    - Updated MessageSchema with `seen` property to track read status of messages.
    - Implemented backend logic to handle `markMessagesAsSeen` event:
      - Updated unseen messages in the database to `seen` when the event is received.
      - Updated the `lastMessage.seen` property in ConversationSchema.
      - Emitted `messagesSeen` event to notify clients of seen messages.
    - Ensured real-time update of message seen status in the frontend by listening to `messagesSeen` event and updating the state.

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,7 +1,9 @@
 const { Server } = require('socket.io');
 const http = require('http');
 const express = require('express');
-const app = express()
+const app = express();
+const MessageSchema = require('../modals/MessageSchema')
+const ConversationSchema = require('../modals/ConversationSchema')
 
 const server = http.createServer(app);
 
@@ -18,7 +20,7 @@ const getRecieverSocketId = (recieverId) => {
 }
 
 io.on("connection", (socket) => {
-    console.log("User connected ", socket.id);
+    console.log("User connected ");
     const userId = socket.handshake.query.userId;
 
     if (userId != "undefined") userSocketMap[userId] = socket.id;
@@ -26,8 +28,24 @@ io.on("connection", (socket) => {
     io.emit("getOnlineUsers", Object.keys(userSocketMap)) // take key convert it to array [1,2,3,45,5]
     // getOnlineUsers [it is event]
 
+    // listen seen event markMessagesAsSeen from MessageContainer
+    socket.on("markMessagesAsSeen", async ({ conversationId, userId }) => {        
+        try {
+            await MessageSchema.updateMany({
+                conversationId: conversationId,
+                seen: false,
+            }, { $set: { seen: true } });
+            await ConversationSchema.updateOne({
+                _id: conversationId,
+            }, { $set: { "lastMessage.seen": true } })
+            io.to(userSocketMap[userId]).emit("messagesSeen", { conversationId })
+        } catch (error) {
+            console.log("Error in Message Seen Function In socket.js -> ", error.message);
+        }
+    })
+
     socket.on("disconnect", () => {
-        console.log("User disconnect ", socket.id);
+        console.log("User disconnect ");
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap)) // take key convert it to array [1,2,3,45,5]
         // update after deleteions
